44883d4826e5c23ada9d5a43709ff1e2928f61e5
Osama-Yousef <osamawalidyousef@gmail.com>
2020-10-17 12:25:25 +0300

    store-profile-route

--- pages/store_profile/[id].js
+++ pages/store_profile/[id].js
@@ -10,12 +10,7 @@ export default function singleView(props){
         <>
         <main>
         <div className="infoDiv">
-        {props.info.map(store =>
-            <>
-            <span onClick={() => router.push('../store_profile/[id].js',`../store_profile/${store.id}`)}>{store.store_name}</span>
-
-            </>
-        )}
+        <p>{props.info.store_name}</p>
         </div>
         </main>
      
@@ -24,7 +19,9 @@ export default function singleView(props){
 }
 export async function getServerSideProps(context){
     const id=context.query.id
-    const res= await fetch(`https://yalla-business-api.herokuapp.com/yalla_business_app/api/v1/stores?search=${id}`)
+    const res= await fetch(`https://yalla-business-api.herokuapp.com/yalla_business_app/api/v1/stores/${id}`)
+    
     const singleData = await res.json();
+    console.log(singleData);
     return {props: {info: singleData}}
 }
