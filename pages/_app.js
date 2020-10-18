efbfccba3f1a36d8219d899a719040c8a139d9d2
mohammedghafri <eng.m.ghafri@gmail.com>
2020-10-18 13:36:29 +0300

    add css files to _app.js

---
+++ pages/_app.js
@@ -0,0 +1,15 @@
+// import '../styles/globals.css'
+// import '../styles/header.css'
+// import '../styles/footer.css'
+// import '../styles/about.css'
+import '../styles/storeProfile.css'
+import '../styles/searchResults.css'
+
+
+
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
