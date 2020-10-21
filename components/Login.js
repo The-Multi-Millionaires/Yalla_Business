abb7b0675ba26968bf142d93141eae71031945fc
Osama-Yousef <osamawalidyousef@gmail.com>
2020-10-21 12:41:59 +0300

    ready login

--- components/Login.js
+++ components/Login.js
@@ -180,15 +180,15 @@ class Login extends Component {
             </div>
             <div className="leftbox">
               <h2 className="title"><span className='span'>WELCOME</span></h2>
-              <p className="desc">pick your perfect <span className='span'>bouquet</span></p>
-              {/* <img className="flower smaller" src="https://image.ibb.co/d5X6pn/1357d638624297b.jpg" alt="1357d638624297b" border="0"/> */}
+              <p className="desc" id="backword">BACK </p>
+              <img className="flower smaller" src="/commentat.png" alt="1357d638624297b" border="0"/>
               <p className="account">have an account?</p>
               <button className="button" id="signin">login</button>
             </div>
             <div className="rightbox">
               <h2 className="title"><span className='span'>HELLO</span></h2>
               <p className="desc"> BECOME A MEMBER <span className='span'>Join Us Now</span></p>
-              <img className="flower" src="/our-logo.png"/>
+              <img className="flower" src="/commentat.png"/>
               <p className="account">don't have an account?</p>
               <button className="button" id="signup">sign up</button>
 
